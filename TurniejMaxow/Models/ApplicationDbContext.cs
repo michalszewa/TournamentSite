aedba341523970c0bdc6e3c183835356969318e0
Michalek <examplee@mail.com>
2019-03-25 19:59:02 +0100

    initial comm

---
+++ TurniejMaxow/Models/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace TurniejMaxow.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+
+        public DbSet<FormResponse> Responses { get; set; }
+    }
+}
---
+++ TurniejMaxow/Models/Repository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+namespace TurniejMaxow.Models
+{
+    public static class Repository
+    {
+        private static List<FormResponse> responses = new List<FormResponse>();
+        public static IEnumerable<FormResponse> Responses
+        {
+            get
+            {
+                return responses;
+            }
+        }
+        public static void AddResponse(FormResponse response)
+        {
+            responses.Add(response);
+        }
+    }
+}
