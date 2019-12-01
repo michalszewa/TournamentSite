6ec73a74cd81fc55fdd191c1d03a8bf356da73d0
Michał Szewczak <michalszewa@gmail.com>
2019-12-01 23:04:31 +0100

    Fix for gitignore v2

---
+++ src/Models/IResponsesRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TurniejMaxow.Models
+{
+    public interface IResponsesRepository
+    {
+        IQueryable<FormResponse> Responses { get; }
+        void Add(FormResponse respon);
+    }
+}
